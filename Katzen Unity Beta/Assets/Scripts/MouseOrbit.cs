88aae84b594acf3e74d5486aebdf73ad2f1caecd
Raul Peres Cardoso <31035821+Kniksisk@users.noreply.github.com>
2018-04-06 21:53:10 -0300

    Atualizacao organizada

---
+++ Katzen Unity Beta/Assets/Scripts/MouseOrbit.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Camera-Control/Mouse Orbit with zoom")]
+
+public class MouseOrbit : MonoBehaviour
+{
+
+	public GameObject[] alvo;
+	public GameObject[] posicoes;
+	private int indice = 0;
+	public float distance = 5.0f;
+	public float xSpeed = 120.0f;
+	public float ySpeed = 120.0f;
+
+	public float yMinLimit = -20f;
+	public float yMaxLimit = 80f;
+
+	public float distanceMin = .5f;
+	public float distanceMax = 15f;
+
+	public float SmoothFactor = 0.5f;
+	private RaycastHit hit;
+
+	private Rigidbody rigidbody;
+
+	public bool RotacionarCam;
+
+	float x = 0.0f;
+	float y = 0.0f;
+
+	// Use this for initialization
+	void Start()
+	{
+		Vector3 angles = transform.eulerAngles;
+		x = angles.y;
+		y = angles.x;
+
+		rigidbody = GetComponent<Rigidbody>();
+
+		// Make the rigid body not change rotation
+		if (rigidbody != null)
+		{
+			rigidbody.freezeRotation = true;
+		}
+	}
+
+	void LateUpdate()
+	{
+		if (alvo[indice])
+		{
+			if (RotacionarCam)
+			{
+				x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+				y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+			y = ClampAngle(y, yMinLimit, yMaxLimit);
+			}
+
+			Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+			distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
+			
+			if (Physics.Linecast(alvo[indice].transform.position, posicoes[indice].transform.position, out hit))
+			{
+				distance -= hit.distance;
+			}
+
+			Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+			Vector3 position = rotation * negDistance + alvo[indice].transform.position;
+
+			transform.rotation = rotation;
+			transform.position = position;
+
+			transform.LookAt(alvo[indice].transform);
+		}
+	}
+
+	public static float ClampAngle(float angle, float min, float max)
+	{
+		if (angle < -360F)
+			angle += 360F;
+		if (angle > 360F)
+			angle -= 360F;
+
+		return Mathf.Clamp(angle, min, max);
+	}
+}
---
+++ Katzen Unity Beta/Assets/Scripts/Scripts Usaveis/PushObjects.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(AudioSource))]
+public class PushObjects : MonoBehaviour
+{
+	public AudioClip soundClip;
+	public float ObMass = 300;
+	public float PushAtMass = 100;
+	public float PushingTime;
+	public float ForceToPush;
+	Rigidbody rb;
+	public float vel;
+	AudioSource AudioPlayer;
+	Vector3 dir;
+
+	Vector3 lastPos;
+	float _PushingTime = 0;
+
+
+	//For setup thing please watch the Demo 
+	//https://www.youtube.com/watch?v=VB9rb5fN97I
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		if (rb == null) return;
+
+		AudioPlayer = GetComponent<AudioSource>();
+		if (soundClip != null)
+		{
+			AudioPlayer.clip = soundClip;
+			AudioPlayer.Stop();
+		}
+		AudioPlayer.volume = 0;
+		AudioPlayer.pitch = 0.5f;
+		rb.mass = ObMass;
+	}
+
+	bool IsMoving()
+	{
+		if (rb.velocity.magnitude > 0.06f)
+		{
+			return true;
+		}
+		return false;
+
+	}
+
+	private void Update()
+	{
+		//F key to Push
+		vel = rb.velocity.magnitude;
+		if (Input.GetKeyUp(KeyCode.F))
+		{
+			rb.isKinematic = true;
+			if (soundClip != null)
+			{
+				AudioPlayer.Stop();
+			}
+
+			AudioPlayer.volume = 0f;
+			AudioPlayer.pitch = 0.2f;
+		}
+
+		if (rb.isKinematic == false)
+		{
+			_PushingTime += Time.deltaTime;
+			if (_PushingTime >= PushingTime)
+			{
+				_PushingTime = PushingTime;
+			}
+
+			rb.mass = Mathf.Lerp(ObMass, PushAtMass, _PushingTime / PushingTime);
+			rb.AddForce(dir * ForceToPush, ForceMode.Force);
+		}
+		else
+		{
+			rb.mass = ObMass;
+			_PushingTime = 0;
+
+		}
+
+		if (IsMoving() == true && rb.isKinematic == false)
+		{
+			if (AudioPlayer.isPlaying == false)
+			{
+				AudioPlayer.Play();
+			}
+
+			StartCoroutine(SoundChangeHigh());
+		}
+		else
+		{
+			StartCoroutine(SoundChangeLow());
+		}
+
+
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+
+			if (Input.GetKey(KeyCode.F))
+			{
+				rb.isKinematic = false;
+
+				dir = collision.contacts[0].point - transform.position;
+				// We then get the opposite (-Vector3) and normalize it
+				dir = -dir.normalized;
+
+			}
+		}
+
+	}
+
+
+	IEnumerator SoundChangeHigh()
+	{
+		if (Input.GetKey(KeyCode.F))
+		{
+			AudioPlayer.volume = Mathf.Lerp(0, 0.5f, PushAtMass / rb.mass);
+			AudioPlayer.pitch = Mathf.Lerp(0.2f, 1f, PushAtMass / rb.mass);
+		}
+		yield return new WaitForSeconds(0.1f);
+
+	}
+	IEnumerator SoundChangeLow()
+	{
+		if (Input.GetKey(KeyCode.F))
+		{
+			AudioPlayer.volume = 1 - Mathf.Lerp(0F, 0.5f, Time.deltaTime);
+			AudioPlayer.pitch = 1 - Mathf.Lerp(0.2f, 1f, Time.deltaTime);
+		}
+
+		yield return new WaitForSeconds(0.1f);
+	}
+
+}
