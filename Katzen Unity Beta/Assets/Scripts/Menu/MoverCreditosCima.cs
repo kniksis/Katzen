2aedea1a8876b7cf789adb01d011b22d4662f891
Raul Peres Cardoso <31035821+Kniksisk@users.noreply.github.com>
2018-06-08 22:49:16 -0300

    *Reupagem Scripts

---
+++ Katzen Unity Beta/Assets/Scripts/Menu/MoverCreditosCima.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverCreditosCima : MonoBehaviour {
+
+	public GameObject textoCreditos;
+	public float velocidadeY;
+	RectTransform texCreRect;
+
+	// Use this for initialization
+	void Start () {
+		texCreRect = textoCreditos.GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (texCreRect.localPosition.y <= 842)
+		{
+			texCreRect.localPosition += new Vector3(0, velocidadeY);
+		}
+		else
+		{
+			texCreRect.localPosition = new Vector3(0, -700);
+		}
+	}
+
+	public void AumentarVelocidade(float velo = 0.0f)
+	{
+		velocidadeY = velo;
+	}
+}
---
+++ Katzen Unity Beta/Assets/Scripts/Usaveis/Raycast3.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast3 : MonoBehaviour {
+
+	static float distance3 = 5;
+
+
+	void Update()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+		{
+			distance3 = hit.distance;
+		}
+	}
+}
