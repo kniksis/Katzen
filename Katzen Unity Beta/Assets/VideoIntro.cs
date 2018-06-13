2d25b5903ba46ec42b854ab52681e6a550d3af6d
Katzen Core Base <kniksiskatora@gmail.com>
2018-06-13 16:19:27 -0300

    Farmacia
    *Box colliders adcionados as cabanas.
    *KitMedico modelo adcionado.
    *Video de introdução beta adcionado.
    *Inicio de farmacia criado.
    *Vitrine loja modelo adcionado
    *Logo farmacia criado.

---
+++ Katzen Unity Beta/Assets/VideoIntro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoIntro : MonoBehaviour
+{
+    [SerializeField]
+    string proximaCena;
+
+    [SerializeField]
+    float tempoProxCena = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tempoProxCena -= Time.deltaTime;
+        if(tempoProxCena <= 0.0f)
+        {
+            Application.LoadLevel(proximaCena);
+        }
+	}
+}
