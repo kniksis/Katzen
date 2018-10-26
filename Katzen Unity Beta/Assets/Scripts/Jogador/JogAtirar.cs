c8d2fa46339fcbb523a0e010d07c4337a1dd7fa9
Raul Peres Cardoso <31035821+Kniksisk@users.noreply.github.com>
2018-10-25 22:52:56 -0300

    Dor e Sofrimento Update
    Troca Rigidbody e Tiro
    
    *Tiro com torreta e estilingue
    *Arvore com parte de equilibrio feita
    *Troca de Rigidbody para character
    *Inicio de programação do pulo pelo animator.
    *Animações de pulo adicionadas.
    
    Idle e Melee Combat
    
    *Animacoes de idle e Melee Combat adcionadas
    
    Dellete de arquivos 2
    
    *Arquivos de texturas, kaokaoa e Katzen desnecessarios deletados

--- Katzen Unity Beta/Assets/Scripts/Jogador/JogAtirar.cs
+++ Katzen Unity Beta/Assets/Scripts/Jogador/JogAtirar.cs
@@ -7,8 +7,10 @@ public class JogAtirar : MonoBehaviour {
 	public float fireRate = 0.5f;
 	public float nextFire = 0.0f;
 	public Transform respawnTiro;
-	public GameObject tiroInimigo;
-	bool fire;
+	public Rigidbody tiro;
+
+    public float forcaAtual;
+    bool fire;
 
 	GameObject gameManagerGO;
 	GameManager gmScript;
@@ -21,16 +23,26 @@ public class JogAtirar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		fire = Input.GetButton("Fire1");
+		fire = Input.GetButton("Atirar");
 
-		/*if (fire && gmScript.municao > 0 && gmScript.InventarioHUD.active != true)
-		{
-			if (Time.time > nextFire)
-			{
-				nextFire = Time.time + fireRate;
-				Instantiate(tiroInimigo, respawnTiro.position, respawnTiro.rotation);
-				gmScript.DecreaseMunicao();
-			}
-		}*/
-	}
+        if (fire)
+        {
+            if (Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                //atirar = true;
+                //GameObject temp = gop.ObterObjeto();
+                //if(temp != null)
+                //{
+                //temp.transform.position = respawnTiro.transform.position;
+                //temp.transform.rotation = respawnTiro.transform.rotation;
+                //temp.SetActive(true);
+
+                Rigidbody tiroInstancia = Instantiate(tiro, respawnTiro.position, respawnTiro.rotation) as Rigidbody;
+
+                tiroInstancia.velocity = forcaAtual * respawnTiro.forward;
+                //}
+            }
+        }
+    }
 }
