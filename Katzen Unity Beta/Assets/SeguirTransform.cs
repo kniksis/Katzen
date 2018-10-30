3743886b1254603d56e17673a14b42dacf30aa2d
Katzen Core Base <kniksiskatora@gmail.com>
2018-10-30 13:51:17 -0200

    Estilingue e Texturas
    *Inicio otimizacao de texturas
    *Script para estilingue seguir o personagem

---
+++ Katzen Unity Beta/Assets/SeguirTransform.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguirTransform : MonoBehaviour {
+    public Transform target;
+
+    private void LateUpdate()
+    {
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+    }
+}
